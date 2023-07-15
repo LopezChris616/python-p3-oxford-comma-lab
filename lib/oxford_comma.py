3248159b66bfde44c6f710455fe059820e32e563
Christian Lopez <lopezchristian1610@gmail.com>
2023-07-15 18:30:47 -0400

    All join tests passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ", ".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) >= 3:
+        items[-1] = f"and {items[-1]}"
+        return ", ".join(items)
+
+            
